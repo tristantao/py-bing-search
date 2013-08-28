afdf5d3ccd3556a0f1204ad9a4b92632ed836898
Michael Gimbel <gimbel0893@gmail.com>
2013-08-28 16:02:01 +0000

    fix for querying api

--- bingsearch.py
+++ bingsearch.py
@@ -1,8 +1,12 @@
+import urllib2
 import requests
 
-URL = 'https://api.datamarket.azure.com/Data.ashx/Bing/SearchWeb/Web?Query=%(query)s&$top=50&$format=json'
+URL = 'https://api.datamarket.azure.com/Bing/Search/v1/Composite' \
+      + '?Sources=%(source)s&Query=%(query)s&$top=50&$format=json'
 API_KEY = 'SECRET_API_KEY'
 
 def request(query, **params):
-    r = requests.get(URL % {'query': query}, auth=('', API_KEY))
-    return r.json['d']['results']
+    url = URL % {'source': urllib2.quote("'web'"),
+                 'query': urllib2.quote("'"+query+"'")}
+    r = requests.get(url, auth=('', API_KEY))
+    return r.json()['d']['results']
