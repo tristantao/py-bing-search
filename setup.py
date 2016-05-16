a9b6cd27ce2946cf557dc96af7df7a9d248e8745
tristantao <tao.tristan@gmail.com>
2016-05-16 10:34:31 -0700

    bumping version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 import platform
 
 DESCRIPTION = "A simple lightweight python wrapper for the Azure Bing Search API."
-VERSION = '0.2.4'
+VERSION = '0.2.5'
 LONG_DESCRIPTION = None
 try:
     LONG_DESCRIPTION = open('README.md').read()
